fac4c5417a4472c7f198caedc88b619afc3b258a
MuhammadZeeshan-Zee <zeeshanarif34101@gmail.com>
2024-03-09 14:35:39 +0500

    updated now

--- src/components/Wedo.jsx
+++ src/components/Wedo.jsx
@@ -2,39 +2,42 @@ import React from 'react'
 
 function Wedo() {
   return (
-    <div className='max-w-1200 bg-mybg text-white'>
-     <h1 className='bg-clip-text text-8xl font-bold text-mycolor'>We do</h1>
+    
+    <div className='max-w-1200 bg-mybg text-white p-3.5'>
+     {/* <h1 className=' invisible bg-clip-text text-8xl font-bold text-mycolor text-center'>We do</h1> */}
+     <h1 className=' invisible sm:visible bg-clip-text text-8xl font-bold text-mycolor text-center'>We do</h1>
      
       
 
-      <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-5 gap-7 font-bold'>
+      <div className='grid grid-cols-2 sm:grid-cols-2  md:grid-cols-5 gap-7 font-bold md:place-items-center'>
       
       <div>  
       <img className='pb-8' src="section4Card1Image.svg" alt="image source not found"></img>
-      <h1 className='max-w-12'>Pitch Deck Design </h1>
+      <h1 className='w-24'>Pitch Deck Design </h1>
       </div>
       
       <div >
       <img className='pb-8' src="section4Card2Image.svg" alt="image source not found"></img>
-     <h1 className='max-w-12'>Branding & Advertising</h1>
+     <h1 className='w-24'>Branding & Advertising</h1>
       </div>
       
       <div>
       <img className='pb-8' src="section4Card3Image.svg" alt="image source not found"></img>
-      <h1 className='max-w-12'>Web & App Design</h1>
+      <h1 className='w-24'>Web & App Design</h1>
       </div>
       
       <div>
       <img className='pb-8' src="section4Card4Image.svg" alt="image source not found"></img>
-      <h1 className='max-w-12'>Content & Marketing</h1>
+      <h1 className='w-24'>Content & Marketing</h1>
       </div>
       <div>
       <img className='pb-8' src="section4Card5Image.svg" alt="image source not found"></img>
-      <h1 className='max-w-12'>Video & Motion Design</h1>
+      <h1 className='w-28'>Video & Motion Design</h1>
       </div>
 
       </div>
     </div>
+    
   )
 }
 
